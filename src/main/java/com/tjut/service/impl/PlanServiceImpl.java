ff7c2b0549ef7f4147f5ff7ecc8959120106d925
zangshuai <2572616212@qq.com>
2021-05-17 15:21:27 +0800

    可以将所有导游和计划信息提交到网页

--- src/main/java/com/tjut/service/impl/PlanServiceImpl.java
+++ src/main/java/com/tjut/service/impl/PlanServiceImpl.java
@@ -1,6 +1,8 @@
 package com.tjut.service.impl;
 
+import com.tjut.dao.GuiderDao;
 import com.tjut.dao.PlanDao;
+import com.tjut.dao.impl.GuiderDaoImpl;
 import com.tjut.dao.impl.PlanDaoImpl;
 import com.tjut.entity.Guider;
 import com.tjut.entity.Plan;
@@ -8,8 +10,7 @@ import com.tjut.entity.WorkTime;
 import com.tjut.entity.WorkTimeTable;
 import com.tjut.service.PlanService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PlanServiceImpl implements PlanService {
     private PlanDao planDao = new PlanDaoImpl();
@@ -38,37 +39,69 @@ public class PlanServiceImpl implements PlanService {
     public List<Plan> findByGId(Integer gid) {
         return null;
     }
-
     @Override
     public List<WorkTimeTable> getAllGuidersWorkTime() {
-        List<Plan> all = planDao.findAll();
+
+        PlanDao planDao = new PlanDaoImpl();
+        GuiderDao guiderDao = new GuiderDaoImpl();
         List<WorkTimeTable> l = new ArrayList<>();
-        List<Integer> guider = new ArrayList<>();
-        for (Plan p : all) {
-            Integer gId = p.getGId();
-            if (guider.contains(gId)) {
-                for (WorkTimeTable w : l) {
-                    if (w.getG().getGId().equals(gId)) {
-                        WorkTime time = new WorkTime();
-                        time.setStart(p.getPStartDate());
-                        time.setEnd(p.getPEndDate());
-                        w.add(time);
-                    }
-                }
-            } else {
-                guider.add(gId);
-                WorkTimeTable workTimeTable = new WorkTimeTable();
-                WorkTime time = new WorkTime();
-                time.setStart(p.getPStartDate());
-                time.setEnd(p.getPEndDate());
-                Guider guider1 = new Guider();
-                workTimeTable.setG(guider1);
-                workTimeTable.add(time);
-                l.add(workTimeTable);
+        List<Guider> all = guiderDao.findAll();
+        for (Guider guider : all) {
+            WorkTimeTable workTimeTable = new WorkTimeTable();
+            workTimeTable.setG(guider);
+            List<Plan> byGId = planDao.findByGId(guider.getGId());
+            List<WorkTime> wt = new ArrayList<>();
+            for (Plan p : byGId) {
+                Date pStartDate = p.getPStartDate();
+                Date pEndDate = p.getPEndDate();
+                WorkTime workTime = new WorkTime();
+                workTime.setStart(pStartDate);
+                workTime.setEnd(pEndDate);
+                wt.add(workTime);
             }
+            workTimeTable.setL(wt);
+            l.add(workTimeTable);
         }
         return l;
     }
+
+//    @Override
+//    public List<WorkTimeTable> getAllGuidersWorkTime() {
+//        List<Plan> all = planDao.findAll();
+//        List<WorkTimeTable> l = new ArrayList<>();
+//        List<Integer> guider = new ArrayList<>();
+//        for (Plan p : all) {
+//            Integer gId = p.getGId();
+//            if (guider.contains(gId)) {
+//                for (WorkTimeTable w : l) {
+//                    if (w.getG().getGId().equals(gId)) {
+//                        WorkTime time = new WorkTime();
+//                        time.setStart(p.getPStartDate());
+//                        time.setEnd(p.getPEndDate());
+//                        w.add(time);
+//                    }
+//                }
+//            } else {
+//                guider.add(gId);
+//                WorkTimeTable workTimeTable = new WorkTimeTable();
+//
+//                WorkTime time = new WorkTime();
+//                time.setStart(p.getPStartDate());
+//                time.setEnd(p.getPEndDate());
+//
+//
+//                Guider guider1 = new Guider();
+//                workTimeTable.setG(guider1);
+//                workTimeTable.add(time);
+//                l.add(workTimeTable);
+//            }
+//        }
+//        for(WorkTimeTable w:l){
+//            List<WorkTime> l1 = w.getL();
+//            l1.sort(Comparator.comparing(WorkTime::getStart));
+//        }
+//        return l;
+//    }
 
 //    1. 程序的输入数据
 //    2. 程序运行后反馈的预期结果
--- src/main/java/com/tjut/web/FindFreeGuiderServlet.java
+++ src/main/java/com/tjut/web/FindFreeGuiderServlet.java
@@ -1,11 +1,22 @@
 package com.tjut.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tjut.entity.WorkTime;
+import com.tjut.entity.WorkTimeTable;
+import com.tjut.service.PlanService;
+import com.tjut.service.impl.PlanServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @WebServlet("/findFreeGuiderServlet")
 public class FindFreeGuiderServlet extends HttpServlet {
@@ -15,6 +26,46 @@ public class FindFreeGuiderServlet extends HttpServlet {
          * 找到所有计划时间和导游
          * 找到所有导游及他们的工作时间
          * */
+
+
+//        Date startTime = new Date();
+//        Date endTime = new Date();
+//        startTime.setTime(Long.parseLong(request.getParameter("startTime")));
+//        endTime.setTime(Long.parseLong(request.getParameter("endTime")));
+        PlanService planService = new PlanServiceImpl();
+        List<WorkTimeTable> allGuidersWorkTime = planService.getAllGuidersWorkTime();
+//        System.out.println(allGuidersWorkTime);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(allGuidersWorkTime);
+        System.out.println(json);
+        response.getWriter().write(json);
+//        for (WorkTimeTable ws : allGuidersWorkTime) {
+//            List<WorkTime> l = ws.getL();
+//            List<Date> times = new ArrayList<>();
+//            Date now = new Date();
+//            times.add(now);
+//            for (WorkTime t : l) {
+//                times.add(t.getStart());
+//                times.add(t.getEnd());
+//            }
+//            times.add(getMonthDate(now, 1));
+//            boolean flag = false;
+//            for (int i = 0; i < times.size() - 1; i++) {
+//                if (times.get(i).compareTo(startTime) > 0 && times.get(i + 1).compareTo(endTime) < 0 && ((i & 1) == 0)) {
+//                    flag = true;
+//                    break;
+//                }
+//            }
+//
+//
+//        }
+    }
+
+    public static Date getMonthDate(Date startDate, int month) {
+        LocalDateTime localDateTime = startDate.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime().plusMonths(month);
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
